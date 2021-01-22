e92184a6608b8ab480e923e3417ce1cfe9574e39
iandyh <mail@andyh.io>
2021-01-22 11:10:58 +0900

    add todo

--- shibuya/scheduler/cloudrun.go
+++ shibuya/scheduler/cloudrun.go
@@ -326,6 +326,8 @@ func (cr *CloudRun) GetPodsMetrics(collectionID, planID int64) (map[string]apiv1
 	return nil, nil
 }
 
+//TODO: what we need is actually get the deployed engines account, not only ready ones.
+// We also need to change this in k8s.go
 func (cr *CloudRun) PodReadyCount(collectionID int64) int {
 	items, err := cr.getEnginesByCollection(collectionID)
 	if err != nil {
