0695930e22028dc6bf70f2c2338ed3344d49f5eb
Heri Rusmanto <ts-heri.rusmanto@rakuten.com>
2025-09-18 20:50:19 +0900

    feat: disabled CGO

--- shibuya/build.sh
+++ shibuya/build.sh
@@ -6,10 +6,10 @@ export GO111MODULE=on
 go mod download
 
 case "$target" in
-    "jmeter") GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/shibuya-agent $(pwd)/engines/jmeter
+    "jmeter") CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/shibuya-agent $(pwd)/engines/jmeter
     ;;
-    "controller") GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/shibuya-controller $(pwd)/controller/cmd
+    "controller") CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/shibuya-controller $(pwd)/controller/cmd
     ;;
     *)
-    GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/shibuya
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/shibuya
 esac
